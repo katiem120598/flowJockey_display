c53311a770109375e94caf58d7d94ffc8c749c81
Katie Mueller <63931373+katiem120598@users.noreply.github.com>
2025-11-20 16:12:12 -0500

    Change lowEnergy threshold for movement scaling

--- sketch.js
+++ sketch.js
@@ -249,7 +249,7 @@ function draw() {
 
       for (let pt of party.shapes) {
         // Apply scaled movement
-        if (lowEnergy > 50) {
+        if (lowEnergy > 5) {
           pt.x += party.xdir * moveScale;
           pt.y += party.ydir * moveScale;
         }
